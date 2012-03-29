d1d05f68444aa0da0754fa591ce0a4b961f4c454
Thomas von Deyen <tvd@magiclabs.de>
2012-03-29 16:49:02 +0200

    Splitted monster import action into parts, so they can be easily overridden

--- lib/csv_magic/controller_actions.rb
+++ lib/csv_magic/controller_actions.rb
@@ -20,36 +20,10 @@ module CSVMagic
 
     def import
       if request.post?
-        # already mapped
-        if params[:fields]
-          create_resource_items_from_csv(resource_class)
-          if @csv_import_errors.empty?
-            flash[:notice] = 'Die Daten wurden erfolgreich importiert.'
-            redirect_to :action => :index
-          else
-            flash[:warning] = "Beim Importieren sind Fehler aufgetreten!"
-            render 'csv_magic/import_errors'
-          end
-        #no mapping yet
-        else
-          @mapper = Importer.new(params, self.class.read_inheritable_attribute(:map_fields_options))
-          @raw_data = @mapper.raw_data
-          render 'csv_magic/mapper'
-        end
+        handle_csv_post_request
       else
-        render 'csv_magic/import'
+        render_csv_import_form
       end
-    rescue InconsistentStateError
-      flash[:warning] = 'unbekannter Fehler.'
-    rescue MissingFileContentsError
-      flash[:warning] = 'Bitte eine CSV-Datei hochladen.'
-      render 'controller_actions/import'
-    rescue ::CSV::MalformedCSVError => e
-      flash[:warning] = 'Fehlerhaft formatierte CSV-Datei: ' + e
-      render 'controller_actions/import'
-    rescue ::Errno::ENOENT
-      flash[:warning] = 'Datei nicht mehr auf dem Server. Bitte erneut hochladen!'
-      render 'controller_actions/import'
     end
 
   private
@@ -69,6 +43,43 @@ module CSVMagic
     def resource_class
       @resource_name ||= self.class.name.gsub(/Controller|Admin/, '').gsub(/:{4}/, '::').singularize
       @resource_class ||= @resource_name.constantize
+    end
+
+    def render_csv_import_form
+      render 'csv_magic/import'
+    end
+
+    def handle_csv_post_request
+      # already mapped
+      if params[:fields]
+        create_resource_items_from_csv(resource_class)
+        if @csv_import_errors.empty?
+          flash[:notice] = 'Die Daten wurden erfolgreich importiert.'
+          redirect_to :action => :index
+        else
+          flash[:warning] = "Beim Importieren sind Fehler aufgetreten!"
+          render 'csv_magic/import_errors'
+        end
+      #no mapping yet
+      else
+        @mapper = Importer.new(params, self.class.read_inheritable_attribute(:map_fields_options))
+        @raw_data = @mapper.raw_data
+        render 'csv_magic/mapper'
+      end
+    rescue MissingFileContentsError
+      flash[:warning] = 'Bitte eine CSV-Datei hochladen.'
+      render_csv_import_form
+    rescue ::CSV::MalformedCSVError => e
+      flash[:warning] = "Fehlerhaft formatierte CSV-Datei: #{e.message}"
+      render_csv_import_form
+    rescue ::Errno::ENOENT
+      flash[:warning] = 'Datei nicht mehr auf dem Server. Bitte erneut hochladen!'
+      render_csv_import_form
+    rescue Exception => e
+      flash[:warning] = e.message
+      logger.error(e.message)
+      logger.error(e.backtrace.join("\n"))
+      render_csv_import_form
     end
 
   end
